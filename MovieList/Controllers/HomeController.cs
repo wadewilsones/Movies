adc2c5c514cf0fbb8262ede862492900c6928cbc
wadewilsones <wadewilsones@gmail.com>
2022-09-07 20:46:07 -0400

    Add project files.

---
+++ MovieList/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using MovieList.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private MovieContext context { get; set; }
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
+        }
+
+
+    }
+}
---
+++ MovieList/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string? Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2999, ErrorMessage = "Must be after 1889")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+        public int? Rating { get; set; }
+    }
+}
